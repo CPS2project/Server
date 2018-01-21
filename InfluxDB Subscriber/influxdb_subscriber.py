95ef79cd260ad1d3dfbe2e684262c323c7645a6c
JulienSulpis <julien.sulpis@gmail.com>
2018-01-21 22:26:45 +0100

    chore: minor changes to the InfluxDB subscriber

--- InfluxDB Subscriber/influxdb_subscriber.py
+++ InfluxDB Subscriber/influxdb_subscriber.py
@@ -2,17 +2,21 @@
 #
 # File: influxdb_subscriber.py
 #
-# Description: MQTT subscriber which receives the metrics sent by the objects on the topic:
-# <building>/<floor>/<room>/<object_type>/<object_name>/metrics/<metrics_name>
-# and the message is the value.
+# ### Description
+# This program is a MQTT subscriber which receives the metrics sent by the objects on the topic:
+# <building>/<floor>/<room>/<object_type>/<object_name>/metrics/<metrics_name> and the message is the value.
 #
-# Features: The topic structure allows to retrieve some metadata about the metrics. All of them are stored as tags associated with the metrics
-# in InfluxDB. The database used is always "cps2_project" and the Measurement (equivalent for table in SQL) is the object type. Thus, all the 
-# objects of a same type have their metrics in one Measurement (=table) and can be easily queried. 
+# ### Features
+# The topic structure allows to retrieve some metadata about the metrics. All of them are stored as tags associated with the metrics in 
+# InfluxDB. The database used is always "cps2_project" and the measurement (equivalent for table in SQL) is the object type. Thus, all the 
+# objects of a same type have their metrics in one measurement (=table) and can be easily queried.
 #
-# Dependencies:
-#   - paho-mqtt (sudo pip3 install paho-mqtt)
-#   - influxdb (sudo apt-get install python3-influxdb)
+# The metrics can be visualized in the Chronograf dashboard. You can also make queries, export the data in csv, create e-mail alerts using 
+# Kapacitor, and more. See the documentation [here](https://docs.influxdata.com/chronograf/v1.4/introduction/getting-started/).
+#
+# ### Dependencies
+# - paho-mqtt (sudo pip3 install paho-mqtt)
+# - influxdb (sudo apt-get install python3-influxdb)
 #
 ####################################################################################################
 
@@ -21,9 +25,6 @@ import datetime
 import time
 
 from influxdb import InfluxDBClient
-
-def on_connect(mqttc, obj, flags, rc):
-    print("rc: " + str(rc))
 
 
 def on_message(client, userdata, msg):
@@ -41,9 +42,10 @@ def on_message(client, userdata, msg):
     try:
         # Convert the string to a float so that it is stored as a number and not a string in the database
         message = float(message)
+        data_type = "numericValue"
     except:
         # The message is not a float, this is not a problem
-        pass
+        data_type = "textValue"
 
     print(str(receiveTime) + ": " + msg.topic + " " + str(message))
 
@@ -51,7 +53,7 @@ def on_message(client, userdata, msg):
         {
             "measurement": object_type,
             "fields": {
-                "value": message
+                data_type: message
             },
             "tags": {
                 "building": building,
@@ -69,23 +71,18 @@ def on_publish(mqttc, obj, mid):
     print("mid: " + str(mid))
 
 
-def on_subscribe(mqttc, obj, mid, granted_qos):
-    print("Subscribed: " + str(mid) + " " + str(granted_qos))
-
-
 def on_log(mqttc, obj, level, string):
     print(string)
 
 
 # Set up a client for InfluxDB
+print("### InfluxDB Client  ###")
 dbclient = InfluxDBClient('localhost', 8086, '', '', 'cps2_project')
 dbclient.create_database('cps2_project')
 
 mqttc = mqtt.Client()
 mqttc.on_message = on_message
-mqttc.on_connect = on_connect
 mqttc.on_publish = on_publish
-mqttc.on_subscribe = on_subscribe
 
 # Uncomment to enable debug messages
 # mqttc.on_log = on_log
